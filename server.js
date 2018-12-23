c6bed8fd45b5e7e47f1d0a77eab1da54c084ce76
Vasu <vaasung@gmail.com>
2018-12-24 00:51:33 +0530

    Added static files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http'),
-  fs = require('fs'),
-  express = require('express')
+    fs = require('fs'),
+    path = require('path'),
+    express = require('express')
 
 require('dotenv').config()
 const blog = express()
@@ -8,16 +9,28 @@ const blog = express()
 console.clear()
 console.log(''.padEnd(180, '#'))
 
+
+blog.engine('html', require('ejs').renderFile)
+blog.set('view engine', 'html')
+blog.set('views', path.join(__dirname, 'client/templates'))
+
+// blog.set('view engine', 'ejs')
+
+blog.use('/cl', express.static(path.resolve(__dirname, 'client')))
+
 blog.get('/', (request, response) => {
-  response.send({
-    name: 'vas'
-  })
+    // response.sendFile(path.resolve(__dirname, 'demoHtml/index.html'))
+    // response.send('888')
+    // response.sendFile(path.resolve(__dirname, 'client/index.html'))
+    response.render('index', {
+        title: 'Wow first ejs file'
+    })
 })
 
-blog.listen(process.env.PORT || 3000, () => {
-  console.log(
-    `App is runing on the 'http(s)://${process.env.HOST}:${process.env.PORT}'`
-  )
+blog.listen(process.env.PORT, () => {
+    console.log(
+        `App is runing on the 'http(s)://${process.env.HOST}:${process.env.PORT}'`
+    )
 })
 
 /*
